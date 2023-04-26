636eff40ced7ffa86914d92ef0ed7e8d3f6f6fe9
Carolista <cjones.renaissancewoman@gmail.com>
2023-04-26 04:15:53 -0500

    added CWC logo and mobile-friendly CJ logo to landing page

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,46 @@
-window.addEventListener("load", () => {
-  
-  setTimeout(() => {
-    document.getElementById("logo-container").classList.remove("preload");
-  }, 300);
+window.addEventListener('load', () => {
+  const cjLogoContainer = document.getElementById('cj-logo-container');
 
+  function isTouchDevice() {
+    return (
+      'ontouchstart' in window ||
+      navigator.maxTouchPoints > 0 ||
+      navigator.msMaxTouchPoints > 0
+    );
+  }
+
+  if (isTouchDevice()) {
+    cjLogoContainer.innerHTML += `
+      <a href="/home.html">
+        <img id="cj-logo" src="/images/CJ-open.png" />
+      </a>
+    `;
+  } else {
+    cjLogoContainer.innerHTML += `
+      <a href="/home.html">
+        <div id="hover-anchor">
+          <div class="text-container">
+            <div id="cee" class="text">
+              C<span id="fn" class="fade-in">aroline</span>
+            </div>
+          </div>
+          <div class="text-container">
+            <div id="jay" class="text">
+              J<span id="ln" class="fade-in">ones</span>
+            </div>
+          </div>
+          <div id="circle-mask">
+            <div id="shape4" class="square"></div>
+            <div id="shape3" class="square"></div>
+            <div id="shape2" class="square"></div>
+            <div id="shape1" class="square"></div>
+          </div>
+        </div>
+      </a>
+    `;
+  }
+
+  setTimeout(() => {
+    cjLogoContainer.classList.remove('preload');
+  }, 300);
 });
