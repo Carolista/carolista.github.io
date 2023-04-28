9100450a310a4705e8543e927720e6f29c283d15
Carolista <cjones.renaissancewoman@gmail.com>
2023-04-28 05:38:48 -0500

    improved nav menu toggling

--- scripts/global.js
+++ scripts/global.js
@@ -36,87 +36,93 @@ function init() {
       </div>
     `;
 
-  // TODO: this logic may not longer be necessary
-	if (page !== "" && page !== "index.html") {
+  // BACKDROP
+  const backdrop = document.querySelector("#backdrop-container");
+  backdrop.innerHTML = `
+    <div id="backdrop-right"></div>
+    <div id="backdrop-center"></div>
+    <div id="backdrop-left"></div>
+  `;
 
-    // BACKDROP
-    const backdrop = document.querySelector("#backdrop-container");
-    backdrop.innerHTML = `
-      <div id="backdrop-right"></div>
-      <div id="backdrop-center"></div>
-      <div id="backdrop-left"></div>
-    `;
+  // HEADER
+  const header = document.querySelector("header");
+  header.innerHTML = headerElements;
 
-		// HEADER
-		const header = document.querySelector("header");
-		header.innerHTML = headerElements;
+  const navBox = document.getElementById("nav-box");
+  const navBkg = document.querySelector("#nav-bkg");
+  const navArrow = document.querySelector("#nav-arrow");
+  const navArrowIcon = document.querySelector("#nav-arrow-icon");
 
-    const navBox = document.getElementById("nav-box");
-    const navBkg = document.querySelector("#nav-bkg");
-
-    // TODO: Make navBox open and close on click only, and fix closing click on left arrow on tab (mobile)
-
-    navBox.addEventListener("mouseenter", () => {
+  navArrow.addEventListener("click", () => {
+    if (navBkg.style.display === "block") {
+      navBkg.style.opacity = 0;
+      navBox.style.transform = null;
+      navArrowIcon.style.transform = "rotate(360deg)";
+      setTimeout(() => {
+        navBkg.style.display = "none"
+      }, 500);
+    } else {
       navBkg.style.display = "block";
+      navBox.style.transform = "translate(240px, 18px) scale(1.15)";
+      navArrowIcon.style.transform = "rotate(180deg)";
       setTimeout(() => {
         navBkg.style.opacity = 0.8;
       }, 100);
-    });
-    navBox.addEventListener("mouseleave", () => {
-      navBkg.style.opacity = 0;
-      setTimeout(() => {
-        navBkg.style.display = "none"
-      }, 500); // time for visual transition to finish before unblocking page content
-    });
+    }
+  });
+  navBkg.addEventListener("click", () => {
+    navBkg.style.opacity = 0;
+    navBox.style.transform = null;
+    navArrowIcon.style.transform = "rotate(360deg)";
+    setTimeout(() => {
+      navBkg.style.display = "none"
+    }, 500);
+  })
 
-		// FOOTER
-		const footer = document.querySelector("footer");
-		footer.innerHTML = `
-      <div>
-        &copy; 2021&nbsp; Caroline R. Jones &nbsp;&bull;&nbsp; St. Louis, MO
-      </div>
-      <div id="footer-links">
-        <a href="https://github.com/Carolista" target="_blank">
-          <span class="tooltip-container">
-            GitHub
-            <span class="tooltip">
-              Check out my repos and contributions
-            </span>
+  // FOOTER
+  const footer = document.querySelector("footer");
+  footer.innerHTML = `
+    <div>
+      &copy; 2021&nbsp; Caroline R. Jones &nbsp;&bull;&nbsp; St. Louis, MO
+    </div>
+    <div id="footer-links">
+      <a href="https://github.com/Carolista" target="_blank">
+        <span class="tooltip-container">
+          GitHub
+          <span class="tooltip">
+            Check out my repos and contributions
           </span>
-        </a> &nbsp;|&nbsp; 
-        <a href="https://www.hackerrank.com/Carolina49a" target="_blank">
-          <span class="tooltip-container">
-            HackerRank
-            <span class="tooltip">
-              View my badges and certificates
-            </span>
+        </span>
+      </a> &nbsp;|&nbsp; 
+      <a href="https://www.hackerrank.com/Carolina49a" target="_blank">
+        <span class="tooltip-container">
+          HackerRank
+          <span class="tooltip">
+            View my badges and certificates
           </span>
-        </a> &nbsp;|&nbsp; 
-        <a href="https://www.linkedin.com/in/carolinerjones/" target="_blank">
-          <span class="tooltip-container">
-            LinkedIn
-            <span class="tooltip">
-              Visit my full profile
-            </span>
+        </span>
+      </a> &nbsp;|&nbsp; 
+      <a href="https://www.linkedin.com/in/carolinerjones/" target="_blank">
+        <span class="tooltip-container">
+          LinkedIn
+          <span class="tooltip">
+            Visit my full profile
           </span>
-        </a> &nbsp;|&nbsp; 
-        <a href="https://docs.google.com/document/d/1k1mPR0vxqAQla466vhOiXAHgIHWvuDXujZZF_OF_SMs" target="_blank">
-          <span class="tooltip-container">
-            Résumé
-            <span class="tooltip">
-              Download from Google Docs
-            </span>
+        </span>
+      </a> &nbsp;|&nbsp; 
+      <a href="https://docs.google.com/document/d/1k1mPR0vxqAQla466vhOiXAHgIHWvuDXujZZF_OF_SMs" target="_blank">
+        <span class="tooltip-container">
+          Résumé
+          <span class="tooltip">
+            Download from Google Docs
           </span>
-        </a>
-      </div>
-    `;
-	} 
+        </span>
+      </a>
+    </div>
+  `;
 
-  // TODO: See if this is still necessary
   setTimeout(() => {
-    if (document.title !== titleEnd.trim()) {
-      document.querySelector("main").style.visibility = "visible";
-    } 
+    document.querySelector("main").style.visibility = "visible";
   }, 200);
 
   document.addEventListener("click", (e) => {
